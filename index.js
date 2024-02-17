fd0e0ce658ec18165a4c5074dd4b89478600c83b
liamgen.js <liamgen.js@proton.me>
2024-02-17 17:38:51 +0100

    ok

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { createServer } = require("node:http");
 const { join } = require("node:path");
 const { Server } = require("socket.io");
 const cors = require('cors');
+var formidable = require('formidable');
 
 const app = express();
 app.use(cors())
@@ -13,9 +14,24 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+app.post("/lgjs-tools-uploads", (req, res) => {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    var oldpath = files.filetoupload.filepath;
+    var newpath = __dirname + "/packages/lgjs-tools-chat/uploads/" + files.filetoupload.originalFilename;
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) throw err;
+      res.write(newpath);
+      res.end();
+    });
+});
+});
+
 server.listen(8000, () => {
   console.log("server running at http://localhost:8000 with CORS");
 });
+
+
 
 io.on("connection", (socket) => {
   console.log("a user connected");
